d8ab01a343b75bfd8359cdf73517c4ab357d9426
alen <yulunxing@126.com>
2017-05-03 17:30:23 +0800

    控制器+视图显示例子

---
+++ api/controllers/TempController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+/*
+* 测试
+* */
+module.exports = {
+  /*
+  * 显示测试页面
+  * */
+  index : function(req, res) {
+    return res.view('temp/index');
+  }
+};
